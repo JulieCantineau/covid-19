b68b4e022eddd1d72509e5b725ef5ac899321a9e
damien-dumont <damien.dumont@student.umons.ac.be>
2020-03-12 22:10:58 +0100

    Bug de remaniement du tableau de donnée

--- R/dataset.R
+++ R/dataset.R
@@ -30,14 +30,17 @@ tab_geo <- tableau[[3]] # evolution par zone
 ### tab_geo = nombre de malade par subdivision geographique (Region, provinces, communes) 
 # Retirer les lignes inutiles
 # Filtrer la colonne ville
-# Séparer la colonne province et le nombre de malades
+# Séparer la colonne province et le nombre de malades  
 
 tab_geo <- select(tab_geo, Régions, Provinces)
 tab_geo <- filter(tab_geo, Provinces != "Région flamande", Provinces != "Région wallonne", Provinces != "—")
 tab_geo <- unique(tab_geo)
-tab_geo <- tab_geo[!grepl("Total", tab_geo$Provinces[tab_geo$Régions != "Région de Bruxelles-Capitale"]),] #supprime les lignes contenant le mot "total" dans la colonne Provinces, seulement si la case régions n'est pas "Région de Bruxelles-Capitale", car c'est la seule donnée pour Bx
-tab_geo <- tab_geo[!grepl("Total", tab_geo$Régions),]
+
+
 tab_geo <- separate(tab_geo, col = "Provinces", into = c("Provinces", "Malades"), sep = ": ")
 tab_geo$Malades <- as.numeric(tab_geo$Malades)
+#tab_geo[!grepl("Total", tab_geo$Provinces[tab_geo$Régions != "Région de Bruxelles-Capitale"]), ] -> tab_geo
+#Pour une raison quelconque, cette ligne ne fonctionne. Elle est sensé supprimer toutes les lignes avec "Total" dans la colonne "Provinces" sauf si "Régions" est Bruxelles capitale. Sauf que non seulement la dernière ligne (total) est toujours là, mais elle supprime "Région wallonne/Lieux inconnus". J'ai essayé plusieurs méthodes différentes sans aucun succès (comme filter, ou même subset).
+
 
 write$csv(tab_geo, file = "data/Tableau_geographique.csv")
